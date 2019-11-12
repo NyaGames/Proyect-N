b8a1c84cb74da390dda5b5675f07790af7882e74
DDChetok <43351928+DDChetok@users.noreply.github.com>
2019-11-12 15:00:56 +0100

    sign in hecho

---
+++ Project-N/Assets/Scripts/Database/DatabaseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DatabaseManager : MonoBehaviour
+{
+    public TMPro.TMP_InputField SignInUsernameInputText;
+    public TMPro.TMP_InputField SignInPasswordInputText;
+    public TMPro.TMP_InputField SignInRepeatPasswordInputText;
+
+    private Mongo db;
+
+    void Start()
+    {
+        db = new Mongo();
+        db.Init();
+    }
+
+    public void InsertNewAccount()
+    {
+        string username = SignInUsernameInputText.text;
+        string password = SignInPasswordInputText.text;
+        string passwordRepeated = SignInRepeatPasswordInputText.text;
+        if (password.Equals(passwordRepeated)) //Si ambas contraseñas están bien escritas, se intenta insertar
+        {
+            Model_Account userAccount = db.InsertAccount(username, password);
+            if (userAccount != null)
+            {
+                Debug.Log("New account created, your username is: " + userAccount.Username +"#"+ userAccount.Discriminator + ". Use it to login!");
+            }
+            else
+            {
+                Debug.Log("Cannot create the account ");
+            }
+        }
+        else{
+            Debug.Log("Passwords mismatched, write it again!");
+        }
+        
+    }
+
+}
