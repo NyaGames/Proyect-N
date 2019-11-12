848d6cae00fb1283801898e8ac37a937bb4f66b4
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-11-12 15:17:26 +0100

    Arreglis

--- Project-N/Assets/Scripts/LoginMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LoginMessage : MonoBehaviour
-{
-    public string Username { set; get; }
-    public string Password { set; get; }
-}
