ddd3ed6f5f709e3304769568222a26ee6266aa82
JeckLag <jaimebarzano1998@gmail.com>
2019-11-19 15:07:16 +0100

    guardar la textura en array para enviar
    Y cosas desas

--- Project-N/Assets/Scripts/CameraWebPhoto.cs
+++ Project-N/Assets/Scripts/CameraWebPhoto.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Unity.Collections;
 using System.IO;
 using UnityEngine.UI;
+using System;
 
 public class CameraWebPhoto : MonoBehaviour
 {
@@ -10,7 +12,9 @@ public class CameraWebPhoto : MonoBehaviour
     WebCamTexture frontCam;
     private Texture defaultBackground;
 
+    
     public RawImage background;
+    public RawImage photoPlane;
     //public AspectRatioFitter fit;
 
     //public GameObject plano2;
@@ -84,29 +88,98 @@ public class CameraWebPhoto : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
             TakePhoto();
     }
+    public static string colorToHex(Color32 color)
+    {
+        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+        return hex;
+    }
+
+    public static Color hexToColor(string hex)
+    {
+        hex = hex.Replace("0x", "");//in case the string is formatted 0xFFFFFF
+        hex = hex.Replace("#", "");//in case the string is formatted #FFFFFF
+        byte a = 255;//assume fully visible unless specified in hex
+        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        //Only use alpha if the string has enough characters
+        if (hex.Length == 8)
+        {
+            a = byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
+        }
+        return new Color32(r, g, b, a);
+    }
+    public static byte[] StringToByteArray(string hex)
+    {
+        int NumberChars = hex.Length;
+        byte[] bytes = new byte[NumberChars / 2];
+        for (int i = 0; i < NumberChars; i += 2)
+            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        return bytes;
+    }
 
     public void TakePhoto()
     {
+        //Texture2D photo = new Texture2D(frontCam.width, frontCam.height);
+        //photo.SetPixels(frontCam.GetPixels());
+        //photo.Apply();
+        Debug.Log("Photo!!");
         Texture2D photo = new Texture2D(frontCam.width, frontCam.height);
         photo.SetPixels(frontCam.GetPixels());
         photo.Apply();
-        Debug.Log("Photo!!");
+        
+        var data = photo.GetRawTextureData<Color32>();
+        string[] hexData= new string[data.Length];
+        Debug.Log("Enviado" + data.Length);
+        NativeArray<Color32> rgbData = new NativeArray<Color32>(data.Length, Allocator.Temp);
+        byte[] sentColors = new byte[data.Length * 3];
+        byte[] sentColors2 = new byte[data.Length * 3];
+
+
+        ////////////ENVIAR DATOS//////////////
+        /*for (int i=0; i < data.Length; i+=3)
+        {
+            sentColors[i] = data[i].r;
+            sentColors[i + 1] = data[i].g;
+            sentColors[i + 2] = data[i].b;
+            
+            //hexData[i] = colorToHex(data[i]);
+
+            //rgbData[i] = hexToColor(hexData[i]);          
+
+        }*/
+
+        byte[] receivedColors = new byte[data.Length * 4];
+        for (int i = 0, j=0; j < data.Length; i+=3, j++)
+        {
+            sentColors2[i] = data[j].r;
+            sentColors2[i + 1] = data[j].g;
+            sentColors2[i + 2] = data[j].b;
+           
+        }
+        ///////////RECIBIR DATOS/////////////     
+        /*for (int i = 0, j = 0; i < receivedColors.Length; i+=4, j+=3)
+        {
+            receivedColors[i] = sentColors[j];
+            receivedColors[i + 1] = sentColors[j + 1];
+            receivedColors[i + 2] = sentColors[j + 2];
+            receivedColors[i + 3] = 255;
+
+        }*/
+        for (int i = 0, j = 0; j < sentColors2.Length; i += 4, j += 3)
+        {
+            receivedColors[i] = sentColors2[j];
+            receivedColors[i + 1] = sentColors2[j + 1];
+            receivedColors[i + 2] = sentColors2[j + 2];
+            receivedColors[i + 3] = 255;
+
+        }
+        Texture2D tex = new Texture2D(frontCam.width, frontCam.height);
+        tex.LoadRawTextureData(receivedColors);
+        tex.Apply();
+        photoPlane.texture = tex;
+        Debug.Log(data.Length);
 
     }
-    /*IEnumerator TakePhoto()  // Start this Coroutine on some button click
-    {
-
-        // NOTE - you almost certainly have to do this here:
-
-        
-        
-        //plano2.GetComponent<Renderer>().material.mainTexture = photo;
-        //Encode to a PNG
-        byte[] bytes = photo.EncodeToPNG();
-        //Write out the PNG. Of course you have to substitute your_path for something sensible
-        //string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "photo.png");
-        File.WriteAllBytes("photo.png", bytes);
-        Debug.Log("nada");
-
-    }*/
+    
 }
