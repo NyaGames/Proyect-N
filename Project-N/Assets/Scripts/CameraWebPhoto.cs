9cbbf5426cfb4421b1ba024a94081a95bec3acaf
JeckLag <jaimebarzano1998@gmail.com>
2019-11-24 05:25:26 +0100

    Foto 128*128
    Creo que esta bien, aunque ha sido parcialmente guarrilla

--- Project-N/Assets/Scripts/CameraWebPhoto.cs
+++ Project-N/Assets/Scripts/CameraWebPhoto.cs
@@ -12,7 +12,7 @@ public class CameraWebPhoto : MonoBehaviour
     WebCamTexture frontCam;
     private Texture defaultBackground;
 
-    
+   
     public RawImage background;
     public RawImage photoPlane;
     //public AspectRatioFitter fit;
@@ -85,130 +85,108 @@ public class CameraWebPhoto : MonoBehaviour
 
         int orient = -frontCam.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
-        if (Input.GetMouseButtonDown(0))
-            TakePhoto();
+       
     }
-    public static string colorToHex(Color32 color)
+    
+    /*public void TakePhoto()
     {
-        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
-        return hex;
-    }
-
-    public static Color hexToColor(string hex)
-    {
-        hex = hex.Replace("0x", "");//in case the string is formatted 0xFFFFFF
-        hex = hex.Replace("#", "");//in case the string is formatted #FFFFFF
-        byte a = 255;//assume fully visible unless specified in hex
-        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
-        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
-        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
-        //Only use alpha if the string has enough characters
-        if (hex.Length == 8)
-        {
-            a = byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
-        }
-        return new Color32(r, g, b, a);
-    }
-    public static byte[] StringToByteArray(string hex)
-    {
-        int NumberChars = hex.Length;
-        byte[] bytes = new byte[NumberChars / 2];
-        for (int i = 0; i < NumberChars; i += 2)
-            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
-        return bytes;
-    }
-
-    public void TakePhoto()
-    {
-        //Texture2D photo = new Texture2D(frontCam.width, frontCam.height);
-        //photo.SetPixels(frontCam.GetPixels());
-        //photo.Apply();
+       
         Debug.Log("Photo!!");
-        Texture2D photo = new Texture2D(frontCam.width, frontCam.height);
-        photo.SetPixels(frontCam.GetPixels());
-        photo.Apply();
-        var data = photo.GetRawTextureData<Color32>();
+        Debug.Log(frontCam.width + " " + frontCam.height);
+       
+        var data2 = frontCam.GetPixels32();
+    
 
-        Color32[,] colorMatrix = new Color32[426, 319];
-        for (int i = 0; i < 426; i++)
+        int aux = 0;
+        int aux2 = 0;
+        Color32[] newData2 = new Color32[(128 * 128)];
+
+        byte[] sentColors3 = new byte[newData2.Length * 3];
+
+        byte[] receivedColors2 = new byte[newData2.Length * 4];
+
+        Color32[] newData = new Color32[newData2.Length];
+        Color32 color;
+
+        for (int j = 0; j < frontCam.height; j++)    
         {
-            for (int j = 0; j < 319; j++)
+            if (j > 56 && j < 184)
+                aux2++;
+            for (int i = 0; i < frontCam.width; i++)
             {
-                colorMatrix[i, j] = data[j * frontCam.width + i];
-                if ((j * frontCam.width + i)==(frontCam.width*frontCam.height-1))
+                if ((i > 96) && (i < 224) && (j > 56) && (j < 184))
                 {
-                    Debug.Log("");
+                    if (i < 96 + aux2)
+                    {
+                        newData2[aux] = data2[(j * frontCam.width) + (i+127)];       
+                    }
+                    else
+                    {
+                        newData2[aux] = data2[(j * frontCam.width) + i];     
+                    }
+                    aux++;   
                 }
             }
-
         }
-        for (int i = 0; i < frontCam.width; i++)
-        {
-            for (int j = 0; j < frontCam.height; j++)
-            {
-                colorMatrix[i, j] = data[j * frontCam.width + i];
-                if ((j * frontCam.width + i) == (frontCam.width * frontCam.height - 1))
-                {
-                    Debug.Log("");
-                }
-            }
-
-        }
-        Debug.Log(colorMatrix[0, 0]+ "Real: " + data[0] + "-______" + colorMatrix[frontCam.width-1, frontCam.height-1] + " : " + data[data.Length-1]);
-
-
-
-        string[] hexData= new string[data.Length];
-        Debug.Log("Enviado" + data.Length);
-        NativeArray<Color32> rgbData = new NativeArray<Color32>(data.Length, Allocator.Temp);
-        byte[] sentColors = new byte[data.Length * 3];
-        byte[] sentColors2 = new byte[data.Length * 3];
+        
+   
+        Debug.Log(aux);
+        Debug.Log(aux2);
+        newData2[16380] = new Color32(0, 0, 255, 255);
+        newData2[8180] = new Color32(255, 0, 0, 255);
+        Debug.Log(newData2[8191]);
 
 
         ////////////ENVIAR DATOS//////////////
-        /*for (int i=0; i < data.Length; i+=3)
+        for (int i = 0, j = 0; j < newData2.Length; i += 3, j++)
         {
-            sentColors[i] = data[i].r;
-            sentColors[i + 1] = data[i].g;
-            sentColors[i + 2] = data[i].b;
-            
-            //hexData[i] = colorToHex(data[i]);
-
-            //rgbData[i] = hexToColor(hexData[i]);          
-
-        }*/
-
-        byte[] receivedColors = new byte[data.Length * 4];
-        for (int i = 0, j=0; j < data.Length; i+=3, j++)
-        {
-            sentColors2[i] = data[j].r;
-            sentColors2[i + 1] = data[j].g;
-            sentColors2[i + 2] = data[j].b;
-           
+            sentColors3[i] = newData2[j].r;
+            sentColors3[i + 1] = newData2[j].g;
+            sentColors3[i + 2] = newData2[j].b;
         }
-        ///////////RECIBIR DATOS/////////////     
-        /*for (int i = 0, j = 0; i < receivedColors.Length; i+=4, j+=3)
+        
+        ///////////RECIBIR DATOS/////////////   
+        for (int i = 0, j = 0; j < sentColors3.Length; i += 4, j += 3)
         {
-            receivedColors[i] = sentColors[j];
-            receivedColors[i + 1] = sentColors[j + 1];
-            receivedColors[i + 2] = sentColors[j + 2];
-            receivedColors[i + 3] = 255;
-
-        }*/
-        for (int i = 0, j = 0; j < sentColors2.Length; i += 4, j += 3)
-        {
-            receivedColors[i] = sentColors2[j];
-            receivedColors[i + 1] = sentColors2[j + 1];
-            receivedColors[i + 2] = sentColors2[j + 2];
-            receivedColors[i + 3] = 255;
+            receivedColors2[i] = sentColors3[j];
+            receivedColors2[i + 1] = sentColors3[j + 1];
+            receivedColors2[i + 2] = sentColors3[j + 2];
+            receivedColors2[i + 3] = 255;
 
         }
-        Texture2D tex = new Texture2D(frontCam.width, frontCam.height);
-        tex.LoadRawTextureData(receivedColors);
+
+        
+        
+        ///////PASARLOS A COLOR32 AGAIN////////////////////
+        for (int i = 0, j = 0; i < receivedColors2.Length; i += 4, j++)
+        {
+            color = new Color32(receivedColors2[i], receivedColors2[i + 1], receivedColors2[i + 2], receivedColors2[i + 3]);
+            newData[j] = color;
+        }
+
+        Debug.Log(newData[8191]);
+        Texture2D tex = new Texture2D(128, 128);
+        tex.SetPixels32(newData);
         tex.Apply();
         photoPlane.texture = tex;
-        Debug.Log(data.Length);
+        
 
-    }
-    
+    }*/
+    public void TakePhotoGuarra()
+    {
+
+        Canvas canvas = FindObjectOfType<Canvas>();
+        float h = canvas.GetComponent<RectTransform>().rect.height;
+        float w = canvas.GetComponent<RectTransform>().rect.width;
+        
+        float x = ((w-128)/2)/w;
+        float y = ((h - 128) / 2) / h;
+
+        float newW = 128 / w;
+        float newH = 128 / h;
+
+        photoPlane.texture = background.texture;
+        photoPlane.uvRect = new Rect(x, y, newW, newH);
+     }
+
 }
