b3d5285dde7a98dae712bc7c45ee5484160647f9
Jaime <43405226+NyaGames@users.noreply.github.com>
2019-11-26 15:57:56 +0100

    leanTween y alguna prueba

---
+++ Project-N/Assets/pruebaLeanTween.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pruebaLeanTween : MonoBehaviour
+{
+    public Sprite[] sprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.play(gameObject.GetComponent<RectTransform>(), sprites).setSpeed(30.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
