9dc4981ce995cd194145cc741c57e374104b2db4
DDChetok <43351928+DDChetok@users.noreply.github.com>
2019-12-04 20:59:07 +0100

    botones para los segundos de la zona

--- Proyect N Remastered/Assets/Scripts/GUI/Buttons/CloseZoneButton.cs
+++ Proyect N Remastered/Assets/Scripts/GUI/Buttons/CloseZoneButton.cs
@@ -8,21 +8,21 @@ using Photon.Pun;
 
 public class CloseZoneButton : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField minsForClosingText;
-    [SerializeField] private TMP_InputField secsForClosingText;
-    [SerializeField] private TMP_InputField minsText;
-    [SerializeField] private TMP_InputField secsText;
-
+    [SerializeField] private TextMeshProUGUI totalSecsToStartClosing;
+    [SerializeField] private TextMeshProUGUI totalSecsCloseDuration;
+    [SerializeField] private Button incrementSecsToStartClosing;
+    [SerializeField] private Button incrementSecsCloseDuration;
     private Button button;
+    int secsToStartClosing;
+    int secsCloseDuration;
 
-    int secs, mins;
-    int secsForClosing, minsForClosing;
-
+    public int incrementToStartClosing;
+    public int incrementCloseDuration;
     public GameObject countDownPrefab;
 
     private void Awake()
     {
-        button = GetComponent<Button>();
+       button = GetComponent<Button>();
     }
 
     public void StartCountdown()
@@ -31,28 +31,44 @@ public class CloseZoneButton : MonoBehaviour
         {
             GamemasterManager.Instance.SendZoneToOtherPlayers();
             GameObject countDown = PhotonNetwork.Instantiate(countDownPrefab.name,Vector3.zero,Quaternion.identity);
-            countDown.GetComponent<CountDown>().Create(mins * 60 + secs, "NEW ZONE! Closes in ", GameManager.Instance.CloseZone);
+            countDown.GetComponent<CountDown>().Create(secsToStartClosing, "NEW ZONE! Closes in ", GameManager.Instance.CloseZone);
             countDown.GetComponent<CountDown>().StartCoundDown();
+
+            totalSecsToStartClosing.text = "0";
+            totalSecsCloseDuration.text = "0";
         }
     }
 
     private void Update()
     {
-        int.TryParse(secsText.text, out secs);
-        int.TryParse(minsText.text, out mins);
+        int.TryParse(totalSecsToStartClosing.text, out secsToStartClosing);
+        int.TryParse(totalSecsCloseDuration.text, out secsCloseDuration);
 
-        int.TryParse(secsForClosingText.text, out secsForClosing);
-        int.TryParse(minsForClosingText.text, out minsForClosing);
-
-        if ((secsForClosing <= 60 && minsForClosing <= 60 && !(minsForClosing == 0 && secsForClosing == 0)) && (secs <= 60 && mins <= 60 && !(mins == 0 && secs == 0)) && GameManager.Instance.gameStarted && (GamemasterManager.Instance.provZone != null && GamemasterManager.Instance.provZone.activeSelf))
+        if ((secsCloseDuration <= 60  && !( secsCloseDuration == 0)) && (secsToStartClosing <= 60  && !( secsToStartClosing == 0)) && GameManager.Instance.gameStarted && (GamemasterManager.Instance.provZone != null && GamemasterManager.Instance.provZone.activeSelf))
         {
-            GamemasterManager.Instance.secsClosingZone = minsForClosing * 60 + secsForClosing;
+            GamemasterManager.Instance.secsClosingZone = secsCloseDuration;
             button.interactable = true;
         }
         else
         {
             button.interactable = false;
         }
+    }
+
+    public void increaseTotalSecsToStartClosing()
+    {
+        int actualSeconds;
+        int.TryParse(totalSecsToStartClosing.text,out actualSeconds);
+
+        totalSecsToStartClosing.text = (actualSeconds + incrementToStartClosing).ToString();
+    }
+
+    public void increaseTotalSecsCloseDuration()
+    {
+        int actualSeconds;
+        int.TryParse(totalSecsCloseDuration.text, out actualSeconds);
+
+        totalSecsCloseDuration.text = (actualSeconds + incrementCloseDuration).ToString();
     }
 
 }
