8a3aecc0fac8f2e01ef073dc0a3836ea8de2230a
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-12-04 20:43:22 +0100

    Permisos

---
+++ Proyect N Remastered/Assets/RequestPermissionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class RequestPermissionScript : MonoBehaviour
+{
+	private void Start()
+	{
+#if PLATFORM_ANDROID
+		if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+		{
+			Permission.RequestUserPermission(Permission.FineLocation);
+		}
+
+		if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+		{
+			Permission.RequestUserPermission(Permission.Camera);
+		}
+#endif
+	}	
+}
