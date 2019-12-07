a43cc180de64f7bdd2fa6abd48cedb80c6cbcc82
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-12-07 01:45:12 +0100

    Menú de opciones terminado e integrado

---
+++ Proyect N Remastered/Assets/ColorModeDropdown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Wilberforce;
+
+public class ColorModeDropdown : MonoBehaviour
+{	
+	public void ChangeColorMode()
+	{
+		int value = GetComponent<TMP_Dropdown>().value;
+		FindObjectOfType<Colorblind>().Type = value;
+		PersistentData.cameraMode = value;
+	}
+
+	public void ChangeLanguage()
+	{
+		FindObjectOfType<LanguageControl>().SetSelectedLanguage(GetComponent<TMP_Dropdown>().value);
+	}
+}
---
+++ Proyect N Remastered/Assets/UpdateColorMode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wilberforce;
+
+public class UpdateColorMode : MonoBehaviour
+{
+	private void Start()
+	{
+		FindObjectOfType<Colorblind>().Type = PersistentData.cameraMode;
+	}
+}
